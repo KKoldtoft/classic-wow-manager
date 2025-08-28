fdbc1edeb099bc93e3441a93f2e0127dd2274609
Kim Koldtoft <koldtoftkim@gmail.com>
2025-08-28 16:26:12 +0200

    Minor fix to history link in header

--- public/top-bar.js
+++ public/top-bar.js
@@ -165,7 +165,6 @@ function injectRulesNavLink() {
         const existingRules = nav.querySelector('a.top-nav-link[href="/rules"]');
         const existingStats = nav.querySelector('a.top-nav-link[href="/stats"]');
         const existingFaq = nav.querySelector('a.top-nav-link[href="/faq"]');
-        const existingHistory = nav.querySelector('a.top-nav-link[href="/history"]');
 
         // Insert Stats link to the left of Rules
         if (!existingStats) {
@@ -196,13 +195,6 @@ function injectRulesNavLink() {
             nav.appendChild(faqLink);
         }
 
-        if (!existingHistory) {
-            const historyLink = document.createElement('a');
-            historyLink.href = '/history';
-            historyLink.className = 'top-nav-link';
-            historyLink.textContent = 'History';
-            nav.appendChild(historyLink);
-        }
     });
 }
 
